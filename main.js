075e464cc8091e10de3a6ab89d654e59aab53bb6
Sachin Kandel <sachinkandel78@gmail.com>
2026-09-24 12:00:14 +0545

    Import typed library and add logic using it (Javascript logic build-part1)

---
+++ main.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".text",{
+    strings: ["Backend Developer", "AI/ML Enthusiast", "Web Developer","Computer Engineer "],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backdelay: 1000,
+    loop:true
+});
+
