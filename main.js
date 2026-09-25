63442e4dd0f4a22531f461e53ae9ec12250f1456
Sachin Kandel <sachinkandel78@gmail.com>
2026-09-25 21:04:21 +0545

    About Section(About me) completed successfully

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text",{
-    strings: ["Backend Developer", "AI/ML Enthusiast", "Web Developer","Computer Engineer "],
+    strings: ["Backend Developer", "AI/ML Enthusiast", "Full Stack Developer","Computer Engineer "],
     typeSpeed: 100,
     backSpeed: 100,
     backdelay: 1000,
